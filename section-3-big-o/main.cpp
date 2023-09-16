5a6d52c6e0c3b69e021cfae53fbb0fd9a7b42b1b
Alex Titov <54718346+alextitov19@users.noreply.github.com>
2023-09-16 14:28:09 -0700

    time

--- section-3-big-o/main.cpp
+++ section-3-big-o/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <ctime>
+#include <chrono>
 
+using namespace std;
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+    auto start = chrono::system_clock::now();
+
+    for (int i = 0; i < 100000000; i++) {
+        int x = 0;
+
+    }    
+    
+    auto end = chrono::system_clock::now();
+    chrono::duration<double> elapsed_seconds = end-start;
+
+
+    cout << "This took " << elapsed_seconds.count() * 1000 << " milliseconds" << endl;
 }
